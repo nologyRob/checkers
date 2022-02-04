9a66e6589a68a1b874b96e51e264ce299800128b
Ommote <olivia.motevalli@gmail.com>
2022-02-04 17:03:45 +0000

    started implementing logic

--- app.js
+++ app.js
@@ -2,12 +2,17 @@
 
 const pieces = document.querySelectorAll(".piece");
 const squaresOnBoard = document.querySelectorAll(".square");
+const board = document.querySelectorAll(".board");
 
 pieces.forEach((piece) => {
-  piece.addEventListener("dragstart", () => {
+  piece.addEventListener("dragstart", (event) => {
+    // event.dataTransfer.setData("Text", event.target.id);
+    console.log(event.target.parentNode.id);
     piece.classList.add("dragging");
   });
-  piece.addEventListener("dragend", () => {
+  piece.addEventListener("dragend", (event) => {
+    // let id = event.dataTransfer.getData("Text");
+    // console.log(id);
     piece.classList.remove("dragging");
   });
 });
@@ -19,3 +24,12 @@ squaresOnBoard.forEach((square) => {
     square.appendChild(piece);
   });
 });
+
+// for (let i = 0; i < 64; i++) {
+//   pieces[i].addEventListener("dragstart", () => {
+//     pieces[i].classList.add("dragging");
+//   });
+//   pieces[i].addEventListener("dragend", () => {
+//     pieces[i].classList.remove("dragging");
+//   });
+// }
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -3,11 +3,16 @@
 // // DRAG MOVEMENT
 var pieces = document.querySelectorAll(".piece");
 var squaresOnBoard = document.querySelectorAll(".square");
+var board = document.querySelectorAll(".board");
 pieces.forEach(function (piece) {
-  piece.addEventListener("dragstart", function () {
+  piece.addEventListener("dragstart", function (event) {
+    // event.dataTransfer.setData("Text", event.target.id);
+    console.log(event.target.parentNode.id);
     piece.classList.add("dragging");
   });
-  piece.addEventListener("dragend", function () {
+  piece.addEventListener("dragend", function (event) {
+    // let id = event.dataTransfer.getData("Text");
+    // console.log(id);
     piece.classList.remove("dragging");
   });
 });
@@ -17,4 +22,11 @@ squaresOnBoard.forEach(function (square) {
     var piece = document.querySelector(".dragging");
     square.appendChild(piece);
   });
-});
+}); // for (let i = 0; i < 64; i++) {
+//   pieces[i].addEventListener("dragstart", () => {
+//     pieces[i].classList.add("dragging");
+//   });
+//   pieces[i].addEventListener("dragend", () => {
+//     pieces[i].classList.remove("dragging");
+//   });
+// }
